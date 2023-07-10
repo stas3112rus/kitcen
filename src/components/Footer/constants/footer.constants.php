2ef7a597d3feb71fb45d856f5c2c59e3cfb7af46
stas yudin <info@evp-web.ru>
2023-07-10 16:03:55 +0300

    edit footer pages

--- src/components/Footer/constants/footer.constants.php
+++ src/components/Footer/constants/footer.constants.php
@@ -17,18 +17,15 @@ function getFooterValue()
 			"Замена фасадов" => getLinkPage("zamena-kuchonnich-fasadov.php", "category"),
 			"Кухни со встроенной техникой" => getLinkPage("vstroennye-kuhni", "category"),
 			"Готовые кухни" => getLinkPage("gotovye-kuhni", "category"),
-			"Вызвать замерщика" => getLinkPage("vyzvat-zamersika.php", "main"),
 			"Кухни с барной стойкой" => getLinkPage("kuhni-s-barnoj-stojokoj", "category")
 		],
 		"columnThird" => [
 			"Выставочные образцы кухонь" => getLinkPage("vysvochnye-obrazi-kuhni", "category"),
 			"Распродажа кухонь" => getLinkPage("rasprodazha-kuhon", "category"),
 			"Акции и скидки на кухни" => getLinkPage("akcii-na-kuhni", "category"),
-			"Про наш интернет-магазин" => getLinkPage("pro-internet-magazin-kuhni-365.php", "main"),
 			"Кухни из массива дерева" => getLinkPage("uhni-iz-massiva", "category"),
 			"Гарантии" => getLinkPage("kuhni-iz-massiva", "category"),
 			"Карта сайта" => getLinkPage("maps.php", "main"),
-			"Написать лично директору" => getLinkPage("napisat-directoru-lichno.php", "main"),
 			"Контакты офиса" => $PAGES['contact'],
 		],
 		"about" => [
